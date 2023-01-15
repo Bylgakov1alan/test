76ab1bd1bd4f1c94fb18175a564343cfc6dcea28
Alan <bulgakov.alan@list.ru>
2023-01-15 20:18:27 +0300

    «6.2»

---
+++ steve/data.js
@@ -0,0 +1,18 @@
+var data = [{
+    title: "стив",
+        picture: "https://rare-gallery.com/uploads/posts/235370-steve-minecraft-1920x1080.jpg",
+        desc: "Главный персонаж игры маинкрафт, его главной задачей является убийство дракона края. Однако после достижения этой цели можно продолжать развиваться и строить различные механизмы и сооружения."
+},
+
+{
+    title: "дракон",
+        picture: "https://phonoteka.org/uploads/posts/2022-09/1663296867_1-phonoteka-org-p-ender-drakon-art-krasivo-1.png",
+        desc: "Главный босс игры Маинкрафт. Обитает в измерении энд мир. Его должен победить стив чтобы пройти игру."
+},
+{
+    title: "визер",
+        picture: "https://i.ytimg.com/vi/6OzbF2u5MhM/maxresdefault.jpg",
+        desc: "Побочный босс. За его убийство дается звезда нижнего мира. Для его призыва нужно 3 головы скелета-иссушителя."
+}];
+
+module.exports.data = data;
