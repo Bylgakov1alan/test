e770ec5fe51c459a3ff499c8dce955e39e518c50
Alan <bulgakov.alan@list.ru>
2023-01-15 20:13:54 +0300

    «6.1»

---
+++ steve/createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("steve");
+database.dropDatabase()
+database = client.db("steve");
+const hunter = database.collection("steve");
+const result = await hunter.insertOne({name:"steve"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
